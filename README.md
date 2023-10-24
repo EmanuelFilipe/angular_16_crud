8a02e216d12f24eb45e98c7cc8b349bcd3ed0859
efilipe <efilipe@magnasistemas.com.br>
2023-10-24 15:47:13 -0300

    implementação de componentes home/products e sistema de rotas

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # angular_9_crud
-executar o comando "npm start" no terminal para iniciar a API backend em node
+- acesse a pasta 'backend' e execute o comando "npm start" no terminal para iniciar a API backend em node
+
+- acesse a pasta 'frontend' e execute o comando ng serve
+
+- criando componentes
+ng g c components/template/footer
---
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent {
+  atributoLegal = "qualquer"
+  
+  fazerAlgo(): void {
+    console.log('fazendo algo!')
+  }
+}
